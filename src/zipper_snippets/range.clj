0fbaf62e79980d267f20e4c4ac2bdc5573abbf90
tkocmathla <matt.t.grimm@gmail.com>
2017-07-13 07:40:33 -0600

    Move copy-zip and cut-zip to range ns and add actual weird zippers

---
+++ src/zipper_snippets/range.clj
@@ -0,0 +1,64 @@
+(ns zipper-snippets.range
+  (:require 
+    [clojure.zip :as z]
+    [zipper-snippets.draw :refer :all]
+    [zipper-snippets.traversal :refer [move-nth]]))
+
+;; copy ------------------------------------------------------------------------
+
+;;          *
+;;        /   \
+;;      :a     *
+;; copy -^   / | \
+;; here    :b :c  *
+;;                | \
+;;                *  :d  <- start here
+;;                |
+;;               :e
+
+(defn copy-zip
+  [loc move-fn & {:keys [replace?]}]
+  (let [node (z/node loc)]
+    (if replace?
+      (-> loc move-fn (z/replace node))
+      (-> loc move-fn z/up (z/append-child node)))))
+
+(let [next-nth (partial move-nth z/next)]
+  (-> (z/vector-zip [:a [:b :c [[:e] :d]]])
+      (next-nth 8)
+      (copy-zip (comp z/left z/up z/up) :replace? true)
+      z/root
+      draw-vec))
+
+;; cut -------------------------------------------------------------------------
+
+;;        *
+;;      /   \
+;;    :a     *
+;;         / | \
+;;       :b :c  *  <- start here
+;;  cut --^ -^  | \
+;; these        *  :d
+;;              |
+;;             :e
+
+(defn root-loc [loc]
+  (->> loc
+       (iterate z/up)
+       (take-while identity)
+       last))
+
+(defn cut-zip
+  [loc move-fns]
+  (->> (reduce (fn [l mf] (z/replace (mf l) :cut)) loc move-fns)
+       root-loc
+       (iterate (fn [l] (if (= :cut (z/node l)) (z/remove l) (z/next l))))
+       (take-while (complement z/end?))
+       last))
+
+(let [next-nth (partial move-nth z/next)]
+  (-> (z/vector-zip [:a [:b :c [[:e] :d]]])
+      (next-nth 5)
+      (cut-zip [z/left z/left])
+      z/root
+      draw-vec))
--- src/zipper_snippets/weird.clj
+++ src/zipper_snippets/weird.clj
@@ -2,64 +2,29 @@
   (:require
     [clojure.walk :as walk]
     [clojure.zip :as z]
-    [zipper-snippets.draw :refer :all]
-    [zipper-snippets.traversal :refer [move-nth]]))
+    [zipper-snippets.traversal :refer [bf-zip]]))
 
-;; copy ------------------------------------------------------------------------
 
-;;          *
-;;        /   \
-;;      :a     *
-;; copy -^   / | \
-;; here    :b :c  *
-;;                | \
-;;                *  :d  <- start here
-;;                |
-;;               :e
+(defn string-zip [root]
+  (z/zipper
+    string?
+    (fn [s] (let [[f & r] s] (if r [f (apply str r)] [f])))
+    identity
+    root))
 
-(defn copy-zip
-  [loc move-fn & {:keys [replace?]}]
-  (let [node (z/node loc)]
-    (if replace?
-      (-> loc move-fn (z/replace node))
-      (-> loc move-fn z/up (z/append-child node)))))
+(->> "bar" string-zip bf-zip)
 
-(let [next-nth (partial move-nth z/next)]
-  (-> (z/vector-zip [:a [:b :c [[:e] :d]]])
-      (next-nth 8)
-      (copy-zip (comp z/left z/up z/up) :replace? true)
-      z/root
-      draw-vec))
 
-;; cut -------------------------------------------------------------------------
+(defn prime-fact-zip [root]
+  (z/zipper
+    #(> % 1)
+    (fn [n] 
+      (loop [d 2]
+        (if (zero? (mod n d)) 
+          (when (not= n d)
+            [(/ n d) d])
+          (recur (inc d)))))
+    identity
+    root))
 
-;;        *
-;;      /   \
-;;    :a     *
-;;         / | \
-;;       :b :c  *  <- start here
-;;  cut --^ -^  | \
-;; these        *  :d
-;;              |
-;;             :e
-
-(defn root-loc [loc]
-  (->> loc
-       (iterate z/up)
-       (take-while identity)
-       last))
-
-(defn cut-zip
-  [loc move-fns]
-  (->> (reduce (fn [l mf] (z/replace (mf l) :cut)) loc move-fns)
-       root-loc
-       (iterate (fn [l] (if (= :cut (z/node l)) (z/remove l) (z/next l))))
-       (take-while (complement z/end?))
-       last))
-
-(let [next-nth (partial move-nth z/next)]
-  (-> (z/vector-zip [:a [:b :c [[:e] :d]]])
-      (next-nth 5)
-      (cut-zip [z/left z/left])
-      z/root
-      draw-vec))
+(-> 102 prime-fact-zip bf-zip)
